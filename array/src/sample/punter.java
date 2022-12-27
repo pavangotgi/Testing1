2d1182d66b1d70839e141814cf6f89e87ff17222
PAVAN <PAVAN@DESKTOP-V6IVHFJ>
2022-12-27 15:21:54 +0530

    Build no 102

---
+++ array/src/sample/punter.java
@@ -0,0 +1,13 @@
+package sample;
+
+public class punter 
+{
+public static void main(String[] args)
+{
+	int[] arr={1,4,1,3,2};
+	for(int i=0;i<arr.length;i++)
+	{
+		System.out.println(arr[i]);
+	}
+}
+}
